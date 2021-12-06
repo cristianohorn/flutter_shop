3d3c4273ac52059877709800b8b20c14f1144f03
Cristiano Horn <cristianohorn@gmail.com>
2021-12-06 13:27:50 -0300

    adicionar função de remover item do cart

---
+++ src/shop/lib/components/cart_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
+
+class CartItemWidget extends StatelessWidget {
+  const CartItemWidget({Key? key, required this.cartItem}) : super(key: key);
+  final CartItem cartItem;
+  @override
+  Widget build(BuildContext context) {
+    double totalPrice = cartItem.price * cartItem.quantity;
+    return Consumer<Cart>(
+      builder: (ctx, cart, _) => Dismissible(
+        key: ValueKey(cartItem.id),
+        background: Container(
+          color: Theme.of(context).errorColor,
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+            size: 40,
+          ),
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 10),
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        ),
+        direction: DismissDirection.endToStart,
+        onDismissed: (_) => {cart.removeItem(cartItem.productId)},
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: FittedBox(
+                    child: Text(
+                      cartItem.price.toStringAsFixed(2),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              title: Text(cartItem.name),
+              subtitle: Text('Total R\$  ${totalPrice.toStringAsFixed(2)}'),
+              trailing: Text('${cartItem.quantity}x'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- src/shop/lib/pages/cart_page.dart
+++ src/shop/lib/pages/cart_page.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/cart_item.dart';
+import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<Cart>(context);
+    final items = provider.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Carrinho"),
       ),
-      body: Text("Carrinho body"),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total', style: TextStyle(fontSize: 20)),
+                  SizedBox(width: 10),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    label: Text(
+                      'R\$ ${provider.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6?.color,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    child: Text('Comprar'),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (ctx, i) => CartItemWidget(
+              cartItem: items[i],
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
