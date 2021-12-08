f9e1365e39b4424d036b13fa184ac3625f8330f2
Cristiano Horn <cristianohorn@gmail.com>
2021-12-07 23:01:40 -0300

    correções de indice

--- src/shop/lib/data/dummy_data.dart
+++ src/shop/lib/data/dummy_data.dart
@@ -3,31 +3,31 @@ import 'package:shop/models/product.dart';
 final dummyProducts = [
   Product(
     id: 'p1',
-    name: 'Red Shirt',
-    description: 'A red shirt - it is pretty red!',
+    name: 'Camiseta',
+    description: 'Linda camiseta vermelha - Tamanhos M|G',
     price: 29.99,
     imageUrl:
         'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
   ),
   Product(
     id: 'p2',
-    name: 'Trousers',
-    description: 'A nice pair of trousers.',
+    name: 'Calça',
+    description: 'Um par de calça jeans.',
     price: 59.99,
     imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
   ),
   Product(
     id: 'p3',
-    name: 'Yellow Scarf',
-    description: 'Warm and cozy - exactly what you need for the winter.',
+    name: 'Cachecol',
+    description: 'Quente, leve e aconhegante - Exatamente o que você precisa no inverno.',
     price: 19.99,
     imageUrl: 'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
   ),
   Product(
     id: 'p4',
-    name: 'A Pan',
-    description: 'Prepare any meal you want.',
+    name: 'Frigideira',
+    description: 'Hora de fritar bacon!',
     price: 49.99,
     imageUrl:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
--- src/shop/lib/models/product_list.dart
+++ src/shop/lib/models/product_list.dart
@@ -33,14 +33,14 @@ class ProductList with ChangeNotifier {
   }
 
   void saveProduct(Map<String, Object> data) {
-    bool hasId = data["id"] != null;
+    bool hasId = data['id'] != null;
 
     final product = Product(
       id: hasId ? data['id'] as String : Random().nextDouble().toString(),
-      name: data["name"].toString(),
-      description: data["description"].toString(),
-      price: data["price"] as double,
-      imageUrl: data["imageUrl"].toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
     );
 
     if (hasId) {
@@ -52,7 +52,7 @@ class ProductList with ChangeNotifier {
 
   void updateProduct(Product product) {
     int index = _items.indexWhere((element) => element.id == product.id);
-    if (index > 0) {
+    if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
@@ -60,7 +60,7 @@ class ProductList with ChangeNotifier {
 
   void removeProduct(Product product) {
     int index = _items.indexWhere((element) => element.id == product.id);
-    if (index > 0) {
+    if (index >= 0) {
       _items.removeWhere((element) => element.id == product.id);
       notifyListeners();
     }
