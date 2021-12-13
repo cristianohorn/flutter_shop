23c4e44336d1d6fb2c6f241c1bc969a41f96e2a9
Cristiano Horn <cristianohorn@gmail.com>
2021-12-12 23:18:58 -0300

    inmplementar função de exclusão e tratamento de erro

--- src/shop/lib/components/product_item.dart
+++ src/shop/lib/components/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/exceptions/http_exception.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -10,56 +11,64 @@ class ProductItem extends StatelessWidget {
   final Product product;
   @override
   Widget build(BuildContext context) {
+    final msg = ScaffoldMessenger.of(context);
     return ListTile(
       leading: CircleAvatar(
         backgroundImage: NetworkImage(product.imageUrl),
       ),
-      title: Text(product.price.toString()),
+      title: Text(product.name),
       trailing: Container(
         width: 100,
         child: Row(
           children: [
             IconButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(
-                    AppRoutes.PRODUCTS_FORM,
-                    arguments: product,
-                  );
-                },
-                icon: Icon(Icons.edit),
-                color: Theme.of(context).colorScheme.primary),
+              icon: Icon(Icons.edit),
+              color: Theme.of(context).colorScheme.primary,
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.PRODUCTS_FORM,
+                  arguments: product,
+                );
+              },
+            ),
             IconButton(
-                onPressed: () {
-                  showDialog<bool>(
-                    context: context,
-                    builder: (ctx) => AlertDialog(
-                      title: Text("Excluir produto"),
-                      content: Text("Tem certeza deseja exclui o produto?"),
-                      actions: [
-                        TextButton(
-                          child: Text("Não"),
-                          onPressed: () {
-                            Navigator.of(ctx).pop(false);
-                          },
+              icon: Icon(Icons.delete),
+              color: Theme.of(context).errorColor,
+              onPressed: () {
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Produto?'),
+                    content: Text('Tem certeza?'),
+                    actions: [
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(ctx).pop(false),
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(ctx).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((value) async {
+                  if (value ?? false) {
+                    try {
+                      await Provider.of<ProductList>(
+                        context,
+                        listen: false,
+                      ).removeProduct(product);
+                    } on HttpException catch (error) {
+                      msg.showSnackBar(
+                        SnackBar(
+                          content: Text(error.toString()),
                         ),
-                        TextButton(
-                          child: Text("Sim"),
-                          onPressed: () {
-                            Navigator.of(ctx).pop(true);
-                          },
-                        ),
-                      ],
-                    ),
-                  ).then((value) => {
-                        if (value ?? false)
-                          Provider.of<ProductList>(
-                            context,
-                            listen: false,
-                          ).removeProduct(product)
-                      });
-                },
-                icon: Icon(Icons.delete),
-                color: Theme.of(context).errorColor),
+                      );
+                    }
+                  }
+                });
+              },
+            ),
           ],
         ),
       ),
--- src/shop/lib/models/product_list.dart
+++ src/shop/lib/models/product_list.dart
@@ -3,12 +3,12 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop/data/dummy_data.dart';
+import 'package:shop/exceptions/http_exception.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _url =
-      'https://hs-store-e33a4-default-rtdb.firebaseio.com/products.json';
+  final _baseUrl =
+      'https://hs-store-e33a4-default-rtdb.firebaseio.com/products';
   final List<Product> _items = [];
   bool _showFavoriteOnly = false;
 
@@ -33,7 +33,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse('$_baseUrl.json'));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -51,7 +51,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     var response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -93,20 +93,42 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((element) => element.id == product.id);
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+        body: jsonEncode(
+          {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+          },
+        ),
+      );
+
       _items[index] = product;
       notifyListeners();
     }
     return Future.value();
   }
 
-  void removeProduct(Product product) {
+  Future<void> removeProduct(Product product) async {
     int index = _items.indexWhere((element) => element.id == product.id);
     if (index >= 0) {
+      final product = _items[index];
       _items.removeWhere((element) => element.id == product.id);
       notifyListeners();
+
+      final response = await http.delete(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+      );
+      if (response.statusCode >= 400) {
+        _items.insert(index, product);
+        notifyListeners();
+        throw HttpException(msg: "Não foi possível excluir o produto!", statusCode: response.statusCode);
+      }
     }
   }
 }
