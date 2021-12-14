dc004fac33017b3f8a2e95a0499774e9ec0d5170
Cristiano Horn <cristianohorn@gmail.com>
2021-12-13 22:02:49 -0300

    adicionando integração de pedidos com firebase

--- src/shop/lib/models/order_list.dart
+++ src/shop/lib/models/order_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 
@@ -14,20 +15,21 @@ class OrderList with ChangeNotifier {
 
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
-    final response = await http.post(Uri.parse('${Constants.ORDER_BASE_URL}.json'),
-        body: jsonEncode({
-          "total": cart.totalAmount,
-          "date": date.toIso8601String(),
-          "products": cart.items.values
-              .map((cartItem) => {
-                    "id": cartItem.id,
-                    "productId": cartItem.productId,
-                    "name": cartItem.name,
-                    "quantity": cartItem.quantity,
-                    "price": cartItem.price,
-                  })
-              .toList(),
-        }));
+    final response =
+        await http.post(Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+            body: jsonEncode({
+              "total": cart.totalAmount,
+              "date": date.toIso8601String(),
+              "products": cart.items.values
+                  .map((cartItem) => {
+                        "id": cartItem.id,
+                        "productId": cartItem.productId,
+                        "name": cartItem.name,
+                        "quantity": cartItem.quantity,
+                        "price": cartItem.price,
+                      })
+                  .toList(),
+            }));
 
     final orderId = jsonDecode(response.body)['name'];
     if (response.statusCode >= 400) {}
@@ -43,5 +45,36 @@ class OrderList with ChangeNotifier {
     );
 
     notifyListeners();
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response =
+        await http.get(Uri.parse('${Constants.ORDER_BASE_URL}.json'));
+    if (response.body == 'null') return;
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          total: orderData['total'],
+          date: DateTime.parse(orderData['date']),
+          products: (orderData['products'] as List<dynamic>).map(
+            (cartItem) {
+              return CartItem(
+                id: cartItem['id'],
+                productId: cartItem['productId'],
+                name: cartItem['name'],
+                quantity: cartItem['quantity'],
+                price: cartItem['price'],
+              );
+            },
+          ).toList(),
+        ),
+      );
+    });
+    notifyListeners();
+    //
   }
 }
