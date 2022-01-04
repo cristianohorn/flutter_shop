17df4e111f006595c24499c0d793c17239f71090
Cristiano Horn <cristianohorn@gmail.com>
2022-01-04 17:07:13 -0300

    conclusão

--- src/shop/lib/components/order.dart
+++ src/shop/lib/components/order.dart
@@ -3,63 +3,77 @@ import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
 class OrderWidget extends StatefulWidget {
-  const OrderWidget({Key? key, required this.order}) : super(key: key);
-
   final Order order;
+  const OrderWidget({
+    Key? key,
+    required this.order,
+  }) : super(key: key);
 
   @override
-  State<OrderWidget> createState() => _OrderWidgetState();
+  _OrderWidgetState createState() => _OrderWidgetState();
 }
 
 class _OrderWidgetState extends State<OrderWidget> {
   bool _expanded = false;
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("R\$ ${widget.order.total.toStringAsFixed(2)}"),
-            subtitle:
-                Text(DateFormat("dd/MM/yyyy HH:mm").format(widget.order.date)),
-            trailing: IconButton(
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: widget.order.products.length * 25 + 10,
-              child: ListView(
-                children: widget.order.products
-                    .map((e) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              e.name,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '${e.quantity}x R\$ ${e.price}',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        ))
-                    .toList(),
+    final itemsHeight = (widget.order.products.length * 24.0) + 10;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
               ),
-            )
-        ],
+              trailing: IconButton(
+                icon: Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded ? itemsHeight : 0,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              child: ListView(
+                children: widget.order.products.map(
+                  (product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.name,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '${product.quantity}x R\$ ${product.price}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ).toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
